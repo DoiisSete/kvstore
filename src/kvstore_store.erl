be7982a344409fad6f10980c263e63a8c4660cb4
DoiisSete <carlosdguerra0@gmail.com>
2026-08-11 21:05:14 +0100

    End of Storage and even the calls

--- src/kvstore_store.erl
+++ src/kvstore_store.erl
@@ -7,7 +7,39 @@
 %%% Created : 11. Aug 2026 15:43
 %%%-------------------------------------------------------------------
 -module(kvstore_store).
--author("fxsclow").
+
+-behaviour(gen_server).
+
+-export([start_link/0, get/1, set/2, delete/1]).
+
+-export([init/1, handle_call/3, handle_cast/2]).
+
+-define(TABLE, kvstore_table).
 
 %% API
--export([]).
+start_link() ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+get(Key) ->
+  case ets:lookup(?TABLE, Key) of
+    [{Key, Value}] -> {ok, Value};
+    [] -> {error, not_found}
+  end.
+
+set(Key, Value) ->
+  ets:insert(?TABLE, {Key, Value}),
+  ok.
+
+delete(Key) ->
+  ets:delete(?TABLE, Key),
+  ok.
+
+init([]) ->
+  ets:new(?TABLE, [set, public, named_table]),
+  {ok, #{}}.
+
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
--- src/kvstore_sup.erl
+++ src/kvstore_sup.erl
@@ -29,7 +29,11 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [
+      #{id => kvstore_store,
+        start => {kvstore_store, start_link, []},
+        type => worker}
+    ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
